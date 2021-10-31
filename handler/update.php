303af7a00efc6d8b67b4ba0d59cd16800c81b8e6
Igor <igornedovic@gmail.com>
2021-10-31 20:57:54 +0100

    Azurirana UPDATE funkcija

---
+++ handler/update.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../dbBroker.php';
+require '../model/prijava.php';
+
+if(isset($_POST['id']) && isset($_POST['katedra']) && isset($_POST['predmet']) 
+&& isset($_POST['sala']) && isset($_POST['datum']))
+{
+    $prijava = new Prijava(null,$_POST['predmet'],$_POST['katedra'],$_POST['sala'],$_POST['datum'] );
+    $status = $prijava->update($_POST['id'], $conn);
+
+    if($status){
+        echo 'Success';
+    }
+    else
+    {
+        echo $status;
+        echo "Failed";
+    }
+}
+
+?> 
